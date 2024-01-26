fecd29f0d82ab6859ecb255265a04c4c8a857fa3
João Chaves <joao.chaves@liferay.com>
2024-01-26 07:55:09 -0300

    feat: create marcacao request dto to transfer request data

---
+++ src/main/java/com/insight/pontoapp/domain/DTO/MarcacaoRequestDTO.java
@@ -0,0 +1,55 @@
+package com.insight.pontoapp.domain.DTO;
+
+import java.time.LocalTime;
+
+public class MarcacaoRequestDTO {
+    private LocalTime entradaManha;
+    private LocalTime saidaManha;
+    private LocalTime entradaTarde;
+    private LocalTime saidaTarde;
+
+    public MarcacaoRequestDTO() {
+    }
+
+    public MarcacaoRequestDTO(LocalTime entradaManha,
+                              LocalTime saidaManha,
+                              LocalTime entradaTarde,
+                              LocalTime saidaTarde) {
+        this.entradaManha = entradaManha;
+        this.saidaManha = saidaManha;
+        this.entradaTarde = entradaTarde;
+        this.saidaTarde = saidaTarde;
+    }
+
+    public LocalTime getEntradaManha() {
+        return entradaManha;
+    }
+
+    public void setEntradaManha(LocalTime entradaManha) {
+        this.entradaManha = entradaManha;
+    }
+
+    public LocalTime getSaidaManha() {
+        return saidaManha;
+    }
+
+    public void setSaidaManha(LocalTime saidaManha) {
+        this.saidaManha = saidaManha;
+    }
+
+    public LocalTime getEntradaTarde() {
+        return entradaTarde;
+    }
+
+    public void setEntradaTarde(LocalTime entradaTarde) {
+        this.entradaTarde = entradaTarde;
+    }
+
+    public LocalTime getSaidaTarde() {
+        return saidaTarde;
+    }
+
+    public void setSaidaTarde(LocalTime saidaTarde) {
+        this.saidaTarde = saidaTarde;
+    }
+}
