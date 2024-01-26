81a5055368e99cb12de848032196798d36f097bc
João Chaves <joao.chaves@liferay.com>
2024-01-26 07:55:31 -0300

    refactor: convert doPost in marcacao servlet to json

--- src/main/java/com/insight/pontoapp/servlets/MarcacoesServlet.java
+++ src/main/java/com/insight/pontoapp/servlets/MarcacoesServlet.java
@@ -1,7 +1,10 @@
 package com.insight.pontoapp.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.insight.pontoapp.data.MarcacoesData;
 import com.insight.pontoapp.data.PeriodoPontoData;
+import com.insight.pontoapp.domain.DTO.MarcacaoRequestDTO;
 import com.insight.pontoapp.domain.models.Marcacao;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,18 +17,25 @@ import java.io.PrintWriter;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet("/marcacoes")
 public class MarcacoesServlet extends HttpServlet {
     private static final long serialVersionUID = 7302329288889146232L;
+    ObjectMapper objectMapper = new ObjectMapper();
 
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException {
-        String entradaStr = request.getParameter("entradaManha");
-        String saidaStr = request.getParameter("saidaManha");
-        String entradaTardeStr = request.getParameter("entradaTarde");
-        String saidaTardeStr = request.getParameter("saidaTarde");
+        objectMapper.registerModule(new JavaTimeModule());
+
+        String requestBody = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        MarcacaoRequestDTO marcacaoJson = objectMapper.readValue(requestBody, MarcacaoRequestDTO.class);
+
+        String entradaStr = marcacaoJson.getEntradaManha().toString();
+        String saidaStr = marcacaoJson.getSaidaManha().toString();
+        String entradaTardeStr = marcacaoJson.getEntradaTarde().toString();
+        String saidaTardeStr = marcacaoJson.getSaidaTarde().toString();
 
         LocalTime entradaManha = LocalTime.parse(entradaStr, DateTimeFormatter.ofPattern("HH:mm"));
         LocalTime saidaManha = LocalTime.parse(saidaStr, DateTimeFormatter.ofPattern("HH:mm"));
@@ -44,6 +54,7 @@ public class MarcacoesServlet extends HttpServlet {
         out.print(buildJsonResponse(MarcacoesData.getMarcacoesData()));
     }
 
+    
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
             throws ServletException, IOException {
