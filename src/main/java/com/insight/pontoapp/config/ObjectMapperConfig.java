3be037a510e189d2d4631742ceaa4aa5f6ed158d
João Chaves <joao.chaves@liferay.com>
2024-01-26 21:28:39 -0300

    feat: create object mapper config class

---
+++ src/main/java/com/insight/pontoapp/config/ObjectMapperConfig.java
@@ -0,0 +1,27 @@
+package com.insight.pontoapp.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class ObjectMapperConfig {
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        objectMapper.setDateFormat(new SimpleDateFormat("HH:mm"));
+
+        objectMapper.registerModule(new SimpleModule().addSerializer(LocalTime.class, new LocalTimeSerializer(timeFormatter)));
+
+        return objectMapper;
+    }
+}
